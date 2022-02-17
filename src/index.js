c2f1904fec13a32511df6015ab25eb56eda27512
bbudzeak <bzezulka@gmail.com>
2022-02-17 17:05:18 -0500

    Added js for forecast
    added js function for forecast HTML

--- src/index.js
+++ src/index.js
@@ -21,6 +21,28 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img src="http://openweathermap.org/img/wn/04d@2x.png" width="42" />
+            <div class="weather-forecast-temperature">
+              <span class="weather-forecast-max">18°</span>
+              <span class="weather-forexcast-min">12°</span>
+            </div>
+          </div>
+        `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -85,6 +107,7 @@ function searchLocation(position) {
 }
 
 search("New York");
+displayForecast();
 
 let celciusTemperature = null;
 
